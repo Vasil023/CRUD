a55552e674ae7414fa46361ff19da1fb73961070
Vasil <vasil@MacBook-Air-Apple.local>
2021-03-08 23:51:14 +0200

    added create user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AddUserForm } from './forms/AddUserForm'
+import { UserTables } from './tables/UserTables'
 
-function App() {
+const App = () => {
+  const userData = [
+    {id: 1, name: 'Vasil', username: 'vasil01'}
+  ]
+
+  const [users, setUsers] = useState(userData)
+
+  const addUser = (user) => {
+    user.id = users.length + 1 
+    setUsers([...users, user])
+  }
+
   return (
-    <h1>Hello</h1>
+    <div className="container">
+      <h1>CRUD App</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+          <AddUserForm 
+          addUser={addUser}
+          />
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTables users={users} />
+        </div>
+      </div>
+    </div>
   )
 }
+
 
 export default App;
