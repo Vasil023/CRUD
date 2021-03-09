66ef681e6ce78face0d93f0cfa68a19036f9a618
Vasil <vasil@MacBook-Air-Apple.local>
2021-03-09 18:16:36 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
 import { AddUserForm } from './forms/AddUserForm'
 import { UserTables } from './tables/UserTables'
+import { EditUserForm} from './forms/EdituserForm'
 
 const App = () => {
   const userData = []
 
   const [users, setUsers] = useState(userData)
+  const [editing, setEditing] = useState(false)
+
+  const initialFormState = {id: null, name: '', username: ''}
+
+  const [currentUser, setCurrentUser] =  useState(initialFormState)
 
   const addUser = (user) => {
     user.id = users.length + 1 
@@ -16,21 +22,46 @@ const App = () => {
     setUsers(users.filter(user => user.id !== id))
   }
 
+  const editRow = (user) => {
+    setEditing(true)
+
+    setCurrentUser({id: user.id, name: user.name, username: user.username})
+  }
+
+  const updateUser = (id, updateUser) => {
+    setEditing(false)
+
+    setUsers(users.map((user) => (user.id === id ? updateUser : user)))
+  }
   return (
     <div className="container">
       <h1>CRUD App</h1>
       <div className="flex-row">
         <div className="flex-large">
-          <h2>Add user</h2>
+          {editing ? (
+            <div>
+            <h2>Edit user</h2>
+            <EditUserForm 
+              setEditing={setEditing}
+              currentUser={currentUser}
+              updateUser={updateUser}
+            />
+            </div>
+          ) : (
+            <div>
+               <h2>Add user</h2>
           <AddUserForm 
           addUser={addUser}
           />
+            </div>
+          )}
         </div>
         <div className="flex-large">
           <h2>View users</h2>
           <UserTables 
           users={users} 
           deleteUser={deleteUser}
+          editRow={editRow}
           />
         </div>
       </div>
--- src/tables/UserTables.js
+++ src/tables/UserTables.js
@@ -17,7 +17,15 @@ const UserTables = props => {
             <td>{user.name}</td>
             <td>{user.username}</td>
             <td>
-              <button className="button muted-button">Edit</button>
+              <button 
+              className="button muted-button"
+              onClick={() => {
+                props.editRow(user)
+              }}
+              >
+                Edit
+              </button>
+
               <button 
               className="button muted-button"
               onClick={() => props.deleteUser(user.id)}
